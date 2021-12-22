21695227525995907e810e7e92cb0803f56ed474
Harri <hello@harrisonmolloy.com>
2021-12-23 00:46:38 +1300

    add slideshow

---
+++ script.js
@@ -0,0 +1,24 @@
+var s = 1;
+var slen = 92;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("loaded");
+  setInterval(next, 3000);
+  document.getElementById("clickarea").addEventListener("click", next);
+});
+
+function next() {
+  if ( s == slen ) { s = 1};
+  s = s + 1;
+  var slide = document.getElementById("slide");
+  var src = "img/HM-Portfolio-" + s + ".png";
+  slide.src = src;
+}
+
+function prev() {
+  if ( s == 1 ) { s = slen + 1 };
+  s = s - 1
+  var slide = document.getElementById("slide");
+  var src = "img/HM-Portfolio-" + s + ".png";
+  slide.src = src;
+}
